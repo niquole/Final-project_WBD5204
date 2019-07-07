88a4d243d92db44ffdfc08fc1534a413473a24a6
niquole <nicole.antonopoulou@gmail.com>
2019-07-07 18:17:01 +0200

    views added

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div id="footer">
+            <p align="right">  &copy; 2019 pet-erinan.com</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    div#footer {
+        border-top: 3px solid #639198;
+        background-color: #e5f6f8;
+        position: absolute;
+        bottom: 0;
+        height: 50px;
+        width: 100%;
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div id="hero"> 
+      <img id="logo" src="../assets/img/logo.png" alt="">
+      <div id="login-header">
+          <a href="">login / sign up</a>
+      </div>
+      <h1 id="name">Pet-erinar</h1>
+    </div>
+
+      <nav id="nav">
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Book</a></li>  
+        </ul>
+      </nav>
+
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  margin: 0;
+
+  div#hero {
+    h1#name {
+      margin: 0;
+      font-size: 20px;
+      color: #639198;
+
+    }
+    img#logo {
+      height: 60px;
+    }
+  }
+
+    nav#nav {
+        border-bottom: 3px solid #639198;
+        border-top: 3px solid #639198;
+        background-color: #e5f6f8;
+
+
+
+      ul {
+         padding: 0;
+         list-style-type: none;
+         li {
+            padding: 0 20px;
+            display: inline-block;
+         }
+         a {
+            font-weight: 700;
+            text-decoration: none;
+            color: #639198;
+
+            }
+        }
+    }
+    div#login-header  {
+            position: absolute;
+            top: 30px;
+            right: 35px;
+            background-color: #E5F6F8;
+            padding: 10px;
+            border: 2px solid #639198;
+        a {
+            text-decoration: none;
+            color: #639198;
+            font-weight: 600;
+        }
+    }
+}
+</style>
